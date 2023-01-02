690495385c005648947145eed34619203b13c106
arkariz <muhammadrisky1401@gmail.com>
2023-01-02 22:20:19 +0700

    feat: add textThemeContext

--- lib/core/theme/theme_constant.dart
+++ lib/core/theme/theme_constant.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:nontonin/core/theme/colors.dart';
 
 ThemeData darkTheme = ThemeData(
+  fontFamily: 'BebasNeue',
   brightness: Brightness.dark,
-  primaryColor: ColorsConstant.blue,
+  primaryColor: ColorsConstant.blueLight,
+  textTheme: TextTheme(
+    titleLarge: TextStyle(
+      fontSize: 55,
+      foreground: Paint()..shader = ColorsConstant.linearGradient,
+    ),
+  ),
 );
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  primaryColor: ColorsConstant.blue,
+  primaryColor: ColorsConstant.blueLight,
 );
+
+TextTheme textThemehemeContext(BuildContext context) {
+  return Theme.of(context).textTheme;
+}
