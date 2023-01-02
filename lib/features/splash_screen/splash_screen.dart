b64c5f7cebfc7aa57b478f03c93187e25892b8ac
arkariz <muhammadrisky1401@gmail.com>
2023-01-02 22:41:18 +0700

    feat: splash screen styling

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:nontonin/core/theme/colors.dart';
 import 'package:nontonin/core/theme/theme_constant.dart';
+import 'package:nontonin/core/utils/size_config.dart';
 import 'package:nontonin/features/splash_screen/splash_screen_controller.dart';
 
 class SplashScreen extends GetView<SplashScreenController> {
@@ -8,11 +10,25 @@ class SplashScreen extends GetView<SplashScreenController> {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig.initialize(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          "NONTONIN",
-          style: textThemehemeContext(context).titleLarge,
+      body: Container(
+        height: SizeConfig.screenHeight,
+        width: SizeConfig.screenWidth,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [
+                ColorsConstant.blackLight,
+                ColorsConstant.blackDark,
+              ]),
+        ),
+        child: Center(
+          child: Text(
+            "NONTONIN",
+            style: textThemehemeContext(context).titleLarge,
+          ),
         ),
       ),
     );
