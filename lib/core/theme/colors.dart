e3f120b62a0ecfef4624a4a5c010ed465924fa35
arkariz <muhammadrisky1401@gmail.com>
2023-01-02 22:20:02 +0700

    feat: add gradient color

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ColorsConstant {
-  static Color blue = const Color(0xFF0296E5);
+  static Shader linearGradient = LinearGradient(
+    colors: <Color>[
+      blueDark,
+      blueLight,
+    ],
+  ).createShader(const Rect.fromLTWH(0.0, 0.0, 400.0, 70.0));
+
+  static Color blueDark = const Color(0xFF19458c);
+  static Color blueLight = const Color(0xFF24539e);
   static Color white = const Color(0xFFFFFFFF);
-  static Color black = const Color(0xFF1E1E1E);
+  static Color black = Colors.black;
   static Color grey = const Color(0xFF92929D);
   static Color orange = const Color(0xFFFF8700);
 }
