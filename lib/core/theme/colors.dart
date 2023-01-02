6b7f23c97cf761465b68f8b8c3a45e3358feda86
arkariz <muhammadrisky1401@gmail.com>
2023-01-02 21:13:10 +0700

    feat: add custom theme

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorsConstant {
+  static Color blue = const Color(0xFF0296E5);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF1E1E1E);
+  static Color grey = const Color(0xFF92929D);
+  static Color orange = const Color(0xFFFF8700);
+}
---
+++ lib/core/theme/theme_constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:nontonin/core/theme/colors.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: ColorsConstant.blue,
+);
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: ColorsConstant.blue,
+);
